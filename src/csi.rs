a920d1125ec1946d777f3d1e08e58b73a55c974f
giz <gizmoloon@gmail.com>
2023-06-05 20:17:26 +0100

    Cleanup.

--- src/csi.rs
+++ src/csi.rs
@@ -10,8 +10,6 @@ use csimsg::{CSIMessage};
 pub const UDP_SERVER_PORT: u16 = 6969;
 pub const UDP_MESSAGE_SIZE: usize = 170;
 
-const CSI_METRICS_MEASUREMENT: &str = "csi_metrics";
-
 #[derive(Error, Debug)]
 pub enum RetrieveCSIError {
     #[error("Could not receive data from UDP socket.")]
@@ -62,10 +60,7 @@ pub fn recv_message(socket: &UdpSocket) -> Result<CSIMessage, RetrieveCSIError> 
 
 pub fn get_csi_measurement(msg: &CSIMessage) -> CSIMeasurement {
     let rssi = i8::try_from(msg.rssi.unwrap()).ok().unwrap();
-
     let timestamp_us = u128::try_from(msg.timestamp.unwrap()).unwrap();
-    // println!("Processing timestamp: {}", timestamp_us);
-    // let timestamp = Timestamp::Microseconds(timestamp_us).into();
 
     // let src_mac = format!("0x{:X}", msg.src_mac.clone().unwrap()[5]);
 
@@ -77,16 +72,13 @@ pub fn get_csi_measurement(msg: &CSIMessage) -> CSIMeasurement {
     for i in (0..128).step_by(2) {
         let real = csi_ints[i];
         let imag = csi_ints[i + 1];
-        // println!("Instantiating complex number with components real: {} and imag: {}", real, imag);
+        
         let compl = num::Complex::new(real as f32, imag as f32);
-        let abs = compl.norm();
 
-        csi_array[ij] = abs;
+        csi_array[ij] = compl.norm();
 
         ij += 1;
     }
-
-    // println!("Array {:?}", csi_array);
     
     CSIMeasurement {
         time: timestamp_us,
